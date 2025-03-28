96cba4fcdd2bbf81ada0e7e864105c4c13a9a95e
uzukidd <uzukidd@gmail.com>
2025-03-28 13:20:52 +0800

    bugfix

--- pcdet/models/roi_heads/roi_head_template.py
+++ pcdet/models/roi_heads/roi_head_template.py
@@ -201,6 +201,10 @@ class RoIHeadTemplate(nn.Module):
         loss_cfgs = self.model_cfg.LOSS_CONFIG
         rcnn_cls = forward_ret_dict['rcnn_cls']
         rcnn_cls_labels = forward_ret_dict['rcnn_cls_labels'].view(-1)
+        rcnn_cls = rcnn_cls[rcnn_cls_labels != -1]
+        rcnn_cls_labels = rcnn_cls_labels[rcnn_cls_labels != -1]
+        
+
         if loss_cfgs.CLS_LOSS == 'BinaryCrossEntropy':
             rcnn_cls_flat = rcnn_cls.view(-1)
             batch_loss_cls = F.binary_cross_entropy(torch.sigmoid(rcnn_cls_flat), rcnn_cls_labels.float(), reduction='none')
